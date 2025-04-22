94552621791c984b555279b12f413e1cf36317c4
Takayuki-Sexy-Suzuki <suzuki@kick-start.co.jp>
2025-04-22 17:16:03 +0900

    Update ga4.js

--- js/ga4.js
+++ js/ga4.js
@@ -5,7 +5,7 @@ function gtag(){ dataLayer.push(arguments); }
 gtag('js', new Date());
 
 // 測定IDだけ書き換えて使ってください
-gtag('config', 'G-XXXXXXXXXX', {
+gtag('config', 'G-SZ5RQR5H7L', {
   anonymize_ip: true,   // IP匿名化（プライバシー保護）
   transport_type: 'beacon'  // パフォーマンス改善
 });
